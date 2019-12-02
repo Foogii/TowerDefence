db422f7e0e4c865bf1236e21987a5cd184a95e34
Colin Labelle <colinlabelle777@gmail.com>
2019-12-02 11:15:26 -0500

    Colin's part 2 (up to: Projectiles)

---
+++ Assets/Scripts/Game/Tower/StoneTower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneTower : Tower
+{
+
+    //1
+    public GameObject stonePrefab;
+    //2
+    protected override void AttackEnemy()
+    {
+        base.AttackEnemy();
+        //3
+        GameObject stone = (GameObject)Instantiate(stonePrefab,
+        towerPieceToAim.position, Quaternion.identity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
