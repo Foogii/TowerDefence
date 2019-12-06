771229b1a72fbdbae72f3c9ec9cce61aaa7d1fbc
Tanner Olsen [Student] <tanner.olsen@student.sl.on.ca>
2019-12-06 13:39:44 -0500

    Added in ice tower, Tanner pt4

--- Assets/Scripts/Game/Enemy/Enemy.cs
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -11,6 +11,10 @@ public class Enemy : MonoBehaviour
 
     private int wayPointIndex = 0;
 
+    public float timeEnemyStaysFrozenInSeconds = 2f;
+    public bool frozen;
+    private float freezeTimer;
+
     void Start()
     {
         EnemyManager.Instance.RegisterEnemy(this);
@@ -53,6 +57,22 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    public void Freeze()
+    {
+        if (!frozen)
+        {
+            frozen = true;
+            moveSpeed /= 2;
+        }
+    }
+
+    void Defrost()
+    {
+        freezeTimer = 0f;
+        frozen = false;
+        moveSpeed *= 2;
+    }
+
     void Update()
     {
         //1
@@ -64,6 +84,16 @@ public class Enemy : MonoBehaviour
         else
         { // 2
             OnGotToLastWayPoint();
+        }
+
+        if(frozen)
+        {
+            freezeTimer += Time.deltaTime;
+
+            if (freezeTimer >= timeEnemyStaysFrozenInSeconds)
+            {
+                Defrost();
+            }
         }
     }
     private void UpdateMovement()
